5a086fcad158410ccf368e9fc2d14b7e0877c77d
Ankit Sood <soodankit1993@gmail.com>
2022-10-16 16:16:10 +0530

    Code to get the promotion rule based on skuId.

--- src/main/java/com/engine/promotion/service/PromotionRuleService.java
+++ src/main/java/com/engine/promotion/service/PromotionRuleService.java
@@ -1,23 +1,23 @@
 package com.engine.promotion.service;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+import com.engine.promotion.model.PromotionRule;
+
 @Service
 public class PromotionRuleService {
-	private Map<String, Double> priceMap;
+	private Map<String, List<PromotionRule>> promotionRulesMap;
 	
 	public PromotionRuleService() {
-		priceMap = new HashMap<>();
-		priceMap.put("A", 50.0);
-		priceMap.put("B", 30.0);
-		priceMap.put("C", 20.0);
-		priceMap.put("D", 15.0);
+
 	}
 	
-	public Double getUnitPrice(String skuId) {
-		return priceMap.getOrDefault(skuId, 0.0);
+	public PromotionRule getPromotionRule(String skuId) {
+		List<PromotionRule> rules = promotionRulesMap.getOrDefault(skuId, new ArrayList<>());
+		return rules.size() != 0 ? rules.get(0) : null;
 	}
 }
