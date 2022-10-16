426116b096c5e7432bbb1692ef11cae9d0c61b99
Ankit Sood <soodankit1993@gmail.com>
2022-10-16 15:46:41 +0530

    Initial Implementation.

--- src/main/java/com/engine/promotion/model/DiscountPercentRuleImpl.java
+++ src/main/java/com/engine/promotion/model/DiscountPercentRuleImpl.java
@@ -2,14 +2,23 @@ package com.engine.promotion.model;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.engine.promotion.service.Promotion;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class DiscountPercentRuleImpl implements Promotion{
+	@Autowired
+	private PriceService priceService;
 
 	@Override
 	public double getOfferPrice(String skuId, PromotionRule rule, Map<String, Integer> productMap) {
-		// TODO Auto-generated method stub
-		return 0;
+		double price = 0.0;
+		int orderedQty = productMap.get(skuId);
+		price = orderedQty  * priceService.getUnitPrice(skuId) * (1 - rule.getOfferPrice());
+		return price;
 	}
 
 }
